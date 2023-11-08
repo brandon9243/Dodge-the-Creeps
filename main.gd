6cd7268df146f7601bcf234bce6ade4e86ea0291
brandon9243 <100967878+brandon9243@users.noreply.github.com>
2023-11-08 14:27:50 -0500

    Created HUD, Finished Main Game Scene
    Finished the Main Game Scene and created HUD for the timer, start button, start game display, and score.

--- main.gd
+++ main.gd
@@ -5,23 +5,28 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
@@ -51,6 +56,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
